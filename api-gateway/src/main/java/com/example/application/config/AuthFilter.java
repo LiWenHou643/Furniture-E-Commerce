6e4e39e10ba05e535c341c25a51d0fef0b04afe5
LiWenHou <quannhan6969@gmail.com>
2024-12-24 19:49:51 +0700

    modify api response in gateway filter

--- api-gateway/src/main/java/com/example/application/config/AuthFilter.java
+++ api-gateway/src/main/java/com/example/application/config/AuthFilter.java
@@ -1,6 +1,9 @@
 package com.example.application.config;
 
+import com.example.application.dto.ApiResponse;
 import com.example.application.service.AuthService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +12,10 @@ import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -25,6 +29,8 @@ public class AuthFilter implements GlobalFilter, Ordered {
 
     AuthService authService;
 
+    ObjectMapper objectMapper;
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("Enter AuthFilter... ");
@@ -35,38 +41,43 @@ public class AuthFilter implements GlobalFilter, Ordered {
         // Check if the header is null or does not start with "Bearer "
         if (CollectionUtils.isEmpty(authHeader) || !authHeader.getFirst().startsWith("Bearer ")) {
             log.error("Authorization header is missing or invalid");
-            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-            return exchange.getResponse().writeWith(Mono.just(
-                    exchange.getResponse().bufferFactory().wrap("Authorization header is missing or invalid".getBytes())
-            ));
+            return unauthenticated(exchange.getResponse());
         }
 
         String token = authHeader.getFirst().replace("Bearer ", "");
 
         // Verify the token
-        authService.introspect(token).subscribe(
-                response -> log.info("Token is {}", response.getData().isValid()),
-                error -> {
-                    if (error instanceof WebClientResponseException webClientError) {
-                        if (webClientError.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-                            log.error("Unauthorized access: {}", webClientError.getMessage());
-                        } else {
-                            log.error("Unexpected error: {}", webClientError.getMessage());
-                        }
-                    } else {
-                        log.error("An error occurred: {}", error.getMessage());
-                    }
-                }
-        );
-
-
-        // Continue to the next filter
-        return chain.filter(exchange);
+        return authService.introspect(token).flatMap(introspectResponse -> {
+            if (introspectResponse.getData().isValid())
+                return chain.filter(exchange);
+            else
+                return unauthenticated(exchange.getResponse());
+        }).onErrorResume(throwable -> unauthenticated(exchange.getResponse()));
     }
 
     @Override
     public int getOrder() {
         return -1;
+    }
+
+    Mono<Void> unauthenticated(ServerHttpResponse response){
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                                                .status("failed")
+                                                .message("Unauthenticated")
+                                                .build();
+
+        String body = null;
+        try {
+            body = objectMapper.writeValueAsString(apiResponse);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+        response.setStatusCode(HttpStatus.UNAUTHORIZED);
+        response.getHeaders().add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        return response.writeWith(
+                Mono.just(response.bufferFactory().wrap(body.getBytes())));
     }
 
 }
