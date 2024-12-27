d72f28438193e3fbf782ea347c391a1a6f6a1455
levanhau <quannhan6969@gmail.com>
2024-12-27 22:57:16 +0700

    auth

--- back-end/src/main/java/com/example/application/controller/CartController.java
+++ back-end/src/main/java/com/example/application/controller/CartController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,16 +21,33 @@ public class CartController {
 
     @GetMapping("/carts/{id}")
     public ResponseEntity<ApiResponse<CartDTO>> getCartById(@PathVariable Long id, Authentication authentication){
-        var x = authentication.getName();
-        var y = authentication.getPrincipal();
-        var z = authentication.getAuthorities();
-        var a = authentication.getCredentials();
-        return ResponseEntity.ok(
+        if (authentication.getPrincipal() instanceof Jwt jwt) {
+            var userId = jwt.getClaim("userId");
+            var cart = cartService.getCartById(id);
+
+            if (userId == null || !userId.equals(cart.getUserId())) {
+                return ResponseEntity.status(403).body(
+                        ApiResponse.<CartDTO>builder()
+                                .status("error")
+                                .message("Unauthorized")
+                                .build()
+                );
+            }
+
+            return ResponseEntity.ok(
+                    ApiResponse.<CartDTO>builder()
+                               .status("success")
+                               .message("Cart fetched")
+                               .data(cartService.getCartById(id))
+                               .build()
+            );
+        }
+
+        return ResponseEntity.status(403).body(
                 ApiResponse.<CartDTO>builder()
-                        .status("success")
-                        .message("Cart fetched")
-                        .data(cartService.getCartById(id))
-                             .build()
+                        .status("error")
+                        .message("Error fetching cart")
+                        .build()
         );
     }
 }
