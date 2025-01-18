5ec38dfc5a6a1f6aa1739efb3840c4113335c503
LiWenHou <quannhan6969@gmail.com>
2025-01-18 13:55:37 +0700

    update kafka topic partition

---
+++ back-end/src/main/java/com/example/application/config/Kafka/KafkaTopicConfig.java
@@ -0,0 +1,15 @@
+package com.example.application.config.Kafka;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic notificationTopic() {
+        // Define topic "notification-delivery" with 3 partitions and replication factor 1
+        return new NewTopic("notification-delivery", 3, (short) 1);
+    }
+}
