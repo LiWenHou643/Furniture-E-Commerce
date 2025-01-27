7b71fca00f2fbc445112b99ab1fe9f9baa283d19
LiWenHou <quannhan6969@gmail.com>
2025-01-27 21:12:11 +0700

    feat(Products): admin search product by keyword name

--- front-end/admin-ui/src/pages/Products.jsx
+++ front-end/admin-ui/src/pages/Products.jsx
@@ -5,58 +5,92 @@ import {
     Box,
     Button,
     Paper,
+    Stack,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
     TableRow,
+    TextField,
     Typography,
 } from '@mui/material';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import { debounce } from 'lodash';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import Error from '../components/Error';
+import Loading from '../components/Loading';
+import useFetchProducts from '../hooks/useFetchProducts';
 
-const initialProducts = [
-    {
-        productId: 2,
-        productName: 'Hobro Don Sofa',
-        productDescription:
-            'The sofa mattress is made of polyester fabric that is dust-proof...',
-        category: { categoryId: 1, categoryName: 'Seating' },
-        brand: { brandId: 5, brandName: 'Herman Miller' },
-        material: { materialId: 3, materialName: 'Fabric' },
-        areas: [
-            { areaId: 2, areaName: 'Bedroom' },
-            { areaId: 1, areaName: 'Living Room' },
-        ],
-        productItems: [
-            {
-                productItemId: 3,
-                color: {
-                    colorId: 1,
-                    colorName: 'Antique Taupe',
-                    hexCode: '#d4c1b1',
-                },
-                sku: 'SEAT02',
-                originalPrice: 100.0,
-                salePrice: 90.0,
-                stockQuantity: 50,
-            },
-        ],
-        averageRating: 4.8,
-        soldQuantity: 4,
-        ratingCount: 178,
-        productStatus: true,
-    },
-];
+import { useEffect, useRef } from 'react';
+
+const itemPerPage = 10;
 
 export default function ProductManagement() {
     const navigate = useNavigate();
-    const [products, setProducts] = useState(initialProducts);
+    const [searchParams, setSearchParams] = useSearchParams();
 
-    const handleDelete = (productId) => {
-        setProducts((prev) => prev.filter((p) => p.productId !== productId));
+    const currentPage = parseInt(searchParams.get('page') || '1', 10);
+
+    const {
+        data: products,
+        isLoading,
+        error,
+    } = useFetchProducts({
+        page: currentPage - 1,
+        size: itemPerPage,
+        categories: searchParams.get('categories'),
+        brands: searchParams.get('brands'),
+        materials: searchParams.get('materials'),
+        minPrice: searchParams.get('minPrice'),
+        maxPrice: searchParams.get('maxPrice'),
+        minRating: searchParams.get('stars'),
+        keyword: searchParams.get('keyword'),
+    });
+
+    // Create a ref to hold the debounced function
+    const debouncedSearchRef = useRef(
+        debounce((keyword) => {
+            const currentParams = Object.fromEntries(searchParams.entries());
+            const updatedParams = { ...currentParams, keyword };
+            setSearchParams(updatedParams);
+            navigate(`?${new URLSearchParams(updatedParams).toString()}`);
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }, 500) // Adjust debounce delay as needed
+    );
+
+    // Handle search input change
+    const handleSearch = (event) => {
+        const keyword = event.target.value;
+        debouncedSearchRef.current(keyword); // Call the debounced function
     };
+
+    // Clean up debounce function on component unmount
+    useEffect(() => {
+        const debouncedFn = debouncedSearchRef.current;
+        return () => {
+            debouncedFn.cancel(); // Cancel any pending debounced calls
+        };
+    }, []);
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
+    if (error) {
+        return <Error error={error} />;
+    }
+
+    // Calculate the page range based on the current page and total items
+    const handlePageChange = (event, page) => {
+        const currentParams = Object.fromEntries(searchParams.entries()); // Get all current search parameters
+        const updatedParams = { ...currentParams, page: page.toString() }; // Merge with the new page number
+        setSearchParams(updatedParams); // Update the search params
+        navigate(`?${new URLSearchParams(updatedParams).toString()}`); // Optionally navigate to the updated URL
+        // Scroll to top of the page
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+    const handleDelete = (productId) => {};
 
     return (
         <Box sx={{ p: 4 }}>
@@ -64,15 +98,35 @@ export default function ProductManagement() {
                 Product Management
             </Typography>
 
-            {/* Add Product Button */}
-            <Button
-                variant='contained'
-                startIcon={<AddIcon />}
-                onClick={() => navigate('/products-management/add')}
-                sx={{ mb: 2 }}
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    flexWrap: 'wrap',
+                    mb: 2,
+                }}
             >
-                Add Product
-            </Button>
+                {/* Add Product Button */}
+                <Button
+                    variant='contained'
+                    startIcon={<AddIcon />}
+                    onClick={() => navigate('/products-management/add')}
+                >
+                    Add Product
+                </Button>
+
+                {/* Search Bar */}
+                <Box sx={{ flexGrow: 1, maxWidth: '600px', ml: 2 }}>
+                    <TextField
+                        fullWidth
+                        label='Search Products'
+                        variant='outlined'
+                        size='small'
+                        onChange={(e) => handleSearch(e)}
+                    />
+                </Box>
+            </Box>
 
             {/* Product Table */}
             <TableContainer component={Paper}>
@@ -82,22 +136,30 @@ export default function ProductManagement() {
                             <TableCell sx={{ fontWeight: 'bold' }}>
                                 Name
                             </TableCell>
-                            <TableCell sx={{ fontWeight: 'bold' }}>
+                            <TableCell
+                                sx={{ fontWeight: 'bold', width: '15%' }}
+                            >
                                 Category
                             </TableCell>
-                            <TableCell sx={{ fontWeight: 'bold' }}>
+                            <TableCell
+                                sx={{ fontWeight: 'bold', width: '15%' }}
+                            >
                                 Brand
                             </TableCell>
-                            <TableCell sx={{ fontWeight: 'bold' }}>
-                                Status
+                            <TableCell
+                                sx={{ fontWeight: 'bold', width: '15%' }}
+                            >
+                                Material
                             </TableCell>
-                            <TableCell sx={{ fontWeight: 'bold' }}>
+                            <TableCell
+                                sx={{ fontWeight: 'bold', width: '15%' }}
+                            >
                                 Actions
                             </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {products.map((product) => (
+                        {products?.content?.map((product) => (
                             <TableRow key={product.productId}>
                                 <TableCell>{product.productName}</TableCell>
                                 <TableCell>
@@ -105,9 +167,7 @@ export default function ProductManagement() {
                                 </TableCell>
                                 <TableCell>{product.brand.brandName}</TableCell>
                                 <TableCell>
-                                    {product.productStatus
-                                        ? 'Available'
-                                        : 'Out of Stock'}
+                                    {product.material.materialName}
                                 </TableCell>
                                 <TableCell>
                                     <EditIcon
@@ -133,6 +193,26 @@ export default function ProductManagement() {
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            {/* Pagination Component */}
+            {
+                // Display pagination component only if there are more than 1 page
+                products?.page?.totalPages > 1 && (
+                    <Stack
+                        spacing={2}
+                        direction='row'
+                        justifyContent='center'
+                        sx={{ marginTop: 3 }}
+                    >
+                        <Pagination
+                            count={Math.ceil(products?.page?.totalPages)} // Total pages
+                            page={currentPage}
+                            onChange={handlePageChange}
+                            color='primary'
+                        />
+                    </Stack>
+                )
+            }
         </Box>
     );
 }
