544fba5608bc421b7fa5b58a34c34217161eec84
levanhau <quannhan6969@gmail.com>
2025-02-02 23:52:00 +0700

    refactor: reformat

--- back-end/src/main/java/com/example/application/constants/AppConstants.java
+++ back-end/src/main/java/com/example/application/constants/AppConstants.java
@@ -2,6 +2,6 @@ package com.example.application.constants;
 
 public final class AppConstants {
     public static final String ROLE_USER = "USER";
-    public static String ROLE_ADMIN = "ADMIN";
-    public static String ROLE_MANAGER = "MANAGER";
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_MANAGER = "MANAGER";
 }
--- back-end/src/main/java/com/example/application/controller/ChatController.java
+++ back-end/src/main/java/com/example/application/controller/ChatController.java
@@ -13,7 +13,6 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -23,7 +22,6 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
-@CrossOrigin(origins = "http://localhost:3001")
 public class ChatController {
 
     SimpMessagingTemplate messagingTemplate;
