af550a5a58241605a79fe007c2acf57255897b50
levanhau <quannhan6969@gmail.com>
2025-02-16 18:18:18 +0700

    refactor: refact order status comment note

--- back-end/src/main/java/com/example/application/constants/OrderStatus.java
+++ back-end/src/main/java/com/example/application/constants/OrderStatus.java
@@ -2,7 +2,7 @@ package com.example.application.constants;
 
 public enum OrderStatus {
     pending,       // Order has been placed, awaiting confirmation
-    processing,     // Payment has been confirmed
+    processing,     // Order has been confirmed and is being processed
     shipped,       // Order has been shipped
     delivered,     // Order has been delivered to the customer
     cancelled,     // Order has been cancelled
