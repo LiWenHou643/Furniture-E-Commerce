2a87d8ae77997e3177b0164bd84ef27007f8bb2b
levanhau <quannhan6969@gmail.com>
2025-03-15 12:52:23 +0700

    fix: make user profile can only be fetch when user logged in

--- front-end/user-ui/src/hooks/useFetchUserProfile.js
+++ front-end/user-ui/src/hooks/useFetchUserProfile.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import axiosPrivate from '../api/axiosPrivate'; // The Axios instance
+import { isAuthenticated } from '../utils/auth';
 
 const fetchUserProfile = async () => {
     const { data } = await axiosPrivate.get('/users/profile');
@@ -8,23 +9,13 @@ const fetchUserProfile = async () => {
 
 const useFetchUserProfile = () => {
     return useQuery(['profile'], fetchUserProfile, {
-        // Keep previous data when fetching new data
         keepPreviousData: true, // Avoid loading state when changing pages
-
-        // Refetch data on window focus
         refetchOnWindowFocus: false, // Avoid refetching data on window focus
-
-        // Refetch data on network reconnect
         refetchOnReconnect: false, // Avoid refetching data on network reconnect
-
-        // Refetch data on interval
         refetchInterval: false, // Avoid refetching data on interval
-
-        // Stale time
         staleTime: 30000, // Set the stale time to 30 seconds
-
-        // Retry failed requests
-        retry: 1, // Set the number of retries to 2
+        retry: 1, // Set the number of retries to 1
+        enabled: isAuthenticated(), // Enable the query if the user is authenticated
     });
 };
 
